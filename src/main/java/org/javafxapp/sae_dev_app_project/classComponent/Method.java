7ccdfafe9775382d6f1f696273dcea128aa0e80e
Adrien M <adrien@mbp-de-adrien.home>
2024-12-18 15:28:03 +0100

    base iteration 2 OK

--- src/main/java/org/javafxapp/sae_dev_app_project/classComponent/Method.java
+++ src/main/java/org/javafxapp/sae_dev_app_project/classComponent/Method.java
@@ -2,28 +2,31 @@ package org.javafxapp.sae_dev_app_project.classComponent;
 
 import javafx.scene.layout.HBox;
 
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+
 public class Method extends ClassComponent {
     private String returnType;
-    private String parameters;
+    private ArrayList<Parameter> parameters;
 
-    public Method(String modifier, String name, String returnType, String parameters) {
+    public Method(String modifier, String name, String returnType) {
         this.modifier = modifier;
         this.name = name;
         this.returnType = returnType;
-        this.parameters = parameters;
+        this.parameters = new ArrayList<>();
     }
 
     public String getReturnType() {
         return returnType;
     }
 
-    public String getParameters() {
+    public ArrayList<Parameter> getParameters() {
         return parameters;
     }
 
     @Override
     public String toString() {
-        return modifier + " " + returnType + " " + name + "(" + parameters + ")";
+        return modifier + " " + returnType + " " + name + "(" + parameters.toString() + ")";
     }
 
     @Override
