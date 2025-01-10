7465d8b8f21bd3da5b290e1a92b3f77a13b7f712
adrienMGN <150789369+adrienMGN@users.noreply.github.com>
2025-01-10 11:22:23 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,13 @@ module org.javafxapp.sae_dev_app_project {
 
     opens org.javafxapp.sae_dev_app_project to javafx.fxml;
     exports org.javafxapp.sae_dev_app_project;
+    exports org.javafxapp.sae_dev_app_project.subjects;
+    exports org.javafxapp.sae_dev_app_project.classComponent;
+    exports org.javafxapp.sae_dev_app_project.importExport;
+    exports org.javafxapp.sae_dev_app_project.menuHandler;
+    exports org.javafxapp.sae_dev_app_project.views;
+
+
+
+    
 }
